7efb7b0dd27fac125c043790e195b5e05eb62c00
Landic <danil.volkov.235@gmail.com>
2024-04-06 03:02:11 +0300

    hw done

---
+++ NinthTask/src/app/cart/cart.component.spec.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CartComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
